0a17adebf266f372f667eb2b8563b39fb713fb2c
bfisch9 <bfisch9@iu.edu>
2018-09-04 17:22:58 -0400

    9/4/2018
    Fixed bug causing ECM vectors to be realigned to either horizontal or vertical only

--- Output/create_quiver_plots_fiber_alignment.m
+++ Output/create_quiver_plots_fiber_alignment.m
@@ -2,10 +2,7 @@
 %output...ECM.mat files are
 
 clear all
-numOutput = 1152;
-images = cell(numOutput, 1);
-
-for i = 0:numOutput
+for i = 0:135
     filename1 = sprintf('output%08u_ECM.mat', i) ;
     ECM = read_ecm_data(filename1);
 %     filename2 = sprintf('output%08u.xml',i) ;
@@ -14,28 +11,27 @@ for i = 0:numOutput
 %     scaled_X_fibers = DATA1.data{1,1}*DATA1.data{1,3};
 %     scaled_Y_fibers = DATA1.data{1,1}*DATA1.data{1,4};
 %     PLOT = quiver(DATA1.X, DATA1.Y,scaled_X_fibers , scaled_Y_fibers);
-    
+    set(figure(1), 'Visible', 'off');
+    figure(1)
     scaled_X_fiber = ECM.data{1,1} .* ECM.data{1,3};
     scaled_Y_fiber = ECM.data{1,1} .* ECM.data{1,4};
-
-    PLOT = figure(1);
-    set(PLOT, 'Visible', 'off');
-    title('ECM Fiber Alignment') ;
-    
     
     quiver(ECM.X, ECM.Y, scaled_X_fiber, scaled_Y_fiber)
-
+%     hold on
+%     contourf(ECM.X,ECM.Y,ECM.data{1,1}) ;
+%     colorbar ;
+%     set(colorbar,'Ylim',[0,1])
     axis image
-%     time = 0;%string(sprintf('%d',DATA2.metadata.current_time/60)) ;
-    
-    filename3 = sprintf('fiber_alignment%08u.png',i) ;
-    saveas(PLOT,filename3) ;
-    
+    time = 0;%string(sprintf('%d',DATA2.metadata.current_time/60)) ;
+%     title({'ECM Fiber Alignment' ; 'Time = ' + time + ' hrs'}) ;
+    filename3 = sprintf('output%08u.png',i) ;
+    saveas(figure(1),filename3) ;
+%     hold off
     images{i + 1} = imread(filename3);
 end
 
 % create the video writer with 1 fps
- writerObj = VideoWriter('fiber_alignment_quiver.mp4');
+ writerObj = VideoWriter('fiber_alignment_wo_cells.mp4');
  writerObj.FrameRate = 60;
 
  % open the video writer
